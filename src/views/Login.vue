73d518f1fe01374185c44efe19c1503c36d2e7c0
Katock <787288968@qq.com>
2022-04-18 22:54:29 +0800

    login_1

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <form class="login-form">
+      <h1>登录</h1>
+      <label><input v-model="id" type="text" placeholder="Id" class="id"></label>
+      <label><input v-model="pwd" type="password" placeholder="Password" class="password"></label>
+      <button type="submit" @click="click_login">登录</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      id: '',
+      pwd: ''
+    }
+  },
+  methods: {
+    click_login() {
+      window.alert("欢迎， " + this.id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
