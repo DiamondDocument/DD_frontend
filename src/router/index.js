1f036c6acb8789d2b8295706484c36d5eed3e31e
evoLonation <1838940019@qq.com>
2022-05-10 12:01:20 +0800

    zzy: 增加相应路由

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,53 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/api/login',
+    path: '/login',
     name: 'login',
-    component: () => import('../views/api/Login.vue')
+    component: () => import('../views/User/Login.vue')
   },
   {
-    path: '/api/register',
+    path: '/register',
     name: 'register',
-    component: () => import('../views/api/Register.vue')
+    component: () => import('../views/User/Register.vue')
   },
   {
-    path: '/api/message',
+    path: '/table/:teamId(\\d+)?',
+    name: 'table',
+    component:() =>import('../views/Table.vue'),
+    children:[
+      {
+        path: 'team/:teamId(\\d+)?',
+        name: 'team',
+        component:() =>import('../views/Team/Team.vue'),
+      },
+      {
+        path: 'team/list/:search',
+        name: 'teamList',
+        component:() =>import('../views/Team/List.vue'),
+      },
+      {
+        path: 'team/create',
+        name: 'teamCreate',
+        component:() =>import('../views/Team/Create.vue'),
+      },
+      {
+        path: 'user/information/:userId?',
+        name: 'userInformation',
+        component:() =>import('../views/User/Information.vue'),
+      },
+      {
+        path: 'template/:type',
+        //type为recommend、my、collection三种
+        name: 'template',
+        component:() =>import('../views/Template/Template.vue'),
+      },
+    ],
+  },
+  {
+    path: '/message',
     name: 'message',
-    component: () => import('../views/api/Message.vue')
-  }
+    component: () => import('../views/Message.vue')
+  },
 ]
 
 const router = createRouter({
