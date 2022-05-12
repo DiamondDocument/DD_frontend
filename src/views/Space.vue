659a123c33b7eea86b8664873b6c2fe252b48245
Katock <787288968@qq.com>
2022-05-12 20:47:25 +0800

    lyh:增加搜索栏

--- src/views/Space.vue
+++ src/views/Space.vue
@@ -1,6 +1,12 @@
 <template>
   <el-menu default-active="'/' +this.$route.path.split('/')[1]">
-    <el-input v-model="input" placeholder="空间内搜索文件"></el-input>
+    <el-input v-model="input" placeholder="空间内搜索文件" style="width: 20%"></el-input>
+    <el-button type="primary" style="margin-left: 10px">
+      <el-icon style="vertical-align: middle;">
+        <search />
+      </el-icon>
+      <span style="vertical-align: middle;">搜索</span>
+    </el-button>
   </el-menu>
   <el-table :data="tableData" height="1000" style="width:100%">
     <el-table-column prop="name" label="文件名" width="500"></el-table-column>
@@ -14,10 +20,11 @@
 <script>
 import Template from "@/views/Template/Template";
 import {ref} from "vue";
+import {Search} from "@element-plus/icons-vue";
 
 export default {
   name: "Space",
-  components: {Template},
+  components: {Search, Template},
   data() {
     return {
       tableData: [
