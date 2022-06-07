e959a3ca07b74c132f3e2174258606e44b643f8e
evoLonation <1838940019@qq.com>
2022-06-07 12:17:36 +0800

    zzy: 完善全局状态

--- src/store/index.js
+++ src/store/index.js
@@ -6,29 +6,34 @@ import {createStore} from 'vuex'
 export default new createStore({
   state(){
     return {
-      isLogin : false,
-      loginUser: {
-        userId: null,
-        nickname: null,
-      },
-      tableType: "",
-      tableId: "",
-      tableInfo: "",
+      isLogin : localStorage.getItem('isLogin'),
+      // loginUser: {
+      //   userId: null,
+      //   nickname: null,
+      // },
+      loginUser : localStorage.getItem('loginUser'),
+      tableInfo: localStorage.getItem('tableInfo'),
+      tableType: localStorage.getItem('tableInfo').split(":")[0],
+      tableId: localStorage.getItem('tableInfo').split(":")[1],
     };
   },
   mutations: {
     login(state, user){
       state.isLogin = true;
-      state.loginUser.userId = user.userId;
-      state.loginUser.nickname = user.nickname;
+      state.loginUser['userId'] = user.userId;
+      state.loginUser['nickname'] = user.nickname;
+      localStorage.setItem('isLogin', state.isLogin);
+      localStorage.setItem('loginUser', JSON.stringify(state.loginUser));
     },
     logout(state){
       state.isLogin = false;
+      localStorage.setItem('isLogin', false);
     },
     changeTable(state, info){
       state.tableInfo = info;
       state.tableId = info.split(":")[1];
       state.tableType = info.split(":")[0];
+      localStorage.setItem('tableInfo', state.tableInfo);
       // if(info.tableId === "my"){
       //   state.tableInfo = "my";
       // }else{
